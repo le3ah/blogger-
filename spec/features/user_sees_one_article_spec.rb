2969ee0fe2e4f023510d1617bead4b1f410817f2
Leah Miller <lkmiller.87@gmail.com>
2018-12-25 12:21:08 -0600

    Adds comment form and passing tests

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -4,7 +4,8 @@ describe  'user sees one article' do
   describe 'they link from the articles index' do
     it "displays information for one article" do
       article_1 = Article.create!(title: "Title 1", body: "Body 1")
-
+      comment_1 = article_1.comments.create(author_name: "Me", body: "Commentary comments")
+      comment_2 = article_1.comments.create(author_name: "You", body: "So much to say")
       visit articles_path
 
       click_link article_1.title
@@ -12,6 +13,10 @@ describe  'user sees one article' do
       expect(current_path).to eq article_path(article_1)
       expect(page).to have_content(article_1.title)
       expect(page).to have_content(article_1.body)
+      expect(page).to have_content(comment_1.author_name)
+      expect(page).to have_content(comment_1.body)
+      expect(page).to have_content(comment_2.author_name)
+      expect(page).to have_content(comment_2.body)
     end
   end
 end
