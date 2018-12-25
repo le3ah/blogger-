6d4973a11444157c71f198f71fccdaa7a67e7616
Leah Miller <lkmiller.87@gmail.com>
2018-12-25 12:41:38 -0600

    Adds ability to show article count

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -17,6 +17,8 @@ describe  'user sees one article' do
       expect(page).to have_content(comment_1.body)
       expect(page).to have_content(comment_2.author_name)
       expect(page).to have_content(comment_2.body)
+      expect(page).to have_content("Comments(2)")
+
     end
   end
   describe  'they fill in a comment form' do
