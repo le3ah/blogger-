49fdd5240830d1991bbbff1d7fe87555c7c415bd
Leah Miller <lkmiller.87@gmail.com>
2018-12-30 07:15:23 -0500

    Adds tag_list method

--- spec/features/user_can_create_new_article_spec.rb
+++ spec/features/user_can_create_new_article_spec.rb
@@ -16,7 +16,7 @@ describe  'user creates new article' do
 
         expect(page).to have_content("New Title!")
         expect(page).to have_content("New Body!")
-        expect(page).to have_content("ruby, technology")
+        expect(page).to have_content("ruby technology")
       end
     end
   end
